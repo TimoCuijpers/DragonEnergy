3d36753b2a0c27b00718e34b8e5b19df45019a87
ps219846 <ps219846@edu.summacollege.nl>
2023-06-26 18:34:41 +0200

    contacten zonder links

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,15 +1,70 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Contact') }}
-        </h2>
-    </x-slot>
+    <div class="py-12 h-full flex-colum">
+        <h1 class="text-left text-body font-bold text-primary">Mail ons</h1>
+        <div class="flex-colum mt-5">
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-welcome />
+            <p class="text-note font-bold">naam</p>
+            <input class="h-9 bg-secondary border-white border-solid border w-full" />
+
+            <p class="text-note font-bold mt-1">Uw e-mailadres</p>
+            <input class="h-9 bg-secondary border-white border-solid border w-full" />
+
+            <p class="text-note font-bold mt-1">Onderwerp</p>
+            <input class="h-9 bg-secondary border-white border-solid border w-full" />
+
+            <p class="text-note font-bold mt-1">Beschrijving</p>
+            <input class=" bg-secondary border-white border-solid border h-32 w-full" />
+        </div>
+
+        <div class="flex mt-7 justify-between">
+            <div>
+            <img src="{{ asset('img/recapatcha.png') }}" class="h-16">
             </div>
+            <div>
+                <button class=" px-9 sm:px-24 py-5 bg-black border border-white">Blikken</button>
+            </div>
+        </div>
+        <div class=" flex-colum mt-14">
+            <h1 class="text-left text-body font-bold text-primary">Openingstijden</h1>
+            <div class="colum-flex mt-10 sm:px-10">
+                <div class="flex text-note font-bold justify-between">
+                    <p>Maandag:</p>
+                    <p>09:00 - 17:00</p>
+                </div>
+                <div class="flex text-note font-bold justify-between mt-5">
+                    <p>Dinsdag:</p>
+                    <p>09:00 - 17:00</p>
+                </div>
+                <div class="flex text-note font-bold justify-between mt-5">
+                    <p>Woensdag:</p>
+                    <p>09:00 - 17:00</p>
+                </div> 
+                <div class="flex text-note font-bold justify-between mt-5">
+                    <p>Donderdag:</p>
+                    <p>09:00 - 17:00</p>
+                </div> 
+                <div class="flex text-note font-bold justify-between mt-5">
+                    <p>Vrijdag:</p>
+                    <p>09:00 - 17:00</p>
+                </div>
+                <div class="flex text-note font-bold justify-between mt-5">
+                    <p>Zaterdag:</p>
+                    <p>Gesloten</p>
+                </div>
+                <div class="flex text-note font-bold justify-between mt-5">
+                    <p>Zondag:</p>
+                    <p>Gesloten</p>
+                </div>
+            </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
         </div>
     </div>
 </x-app-layout>
